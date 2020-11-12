43c33c18fe59aa3a3046ed3ad50eb6d4afbe1ece
Lotinex <loard969@gmail.com>
2020-11-12 20:31:21 +0900

    test

--- src/Main.ts
+++ src/Main.ts
@@ -7,7 +7,9 @@ import Path from 'path';
 const App = Express();
 
 App.use(Express.static(Path.resolve(__dirname, 'public')));
-
+App.get('/', (req, res) => {
+    res.send('Hi, there!')
+})
 App.listen(80, () => {
     console.log('Web Server Opened.')
 })
